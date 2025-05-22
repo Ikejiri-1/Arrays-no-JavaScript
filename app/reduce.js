9aa0f04f46fe15d29d5c4c756d4cadcc560bab48
Ikejiri-1 <pedroike21@gmail.com>
2025-05-22 18:53:18 -0300

    Update reduce.js

--- app/reduce.js
+++ app/reduce.js
@@ -1,6 +1,3 @@
-import {livros} from './main.js'
-import { filtrarLivros } from './filter.js'
-
        export function calcularValorTotalDeLivrosDisponiveis(livros){
        return livros.reduce((acc, livro)=> acc + livro.preco , 0).toFixed(2)
     }
